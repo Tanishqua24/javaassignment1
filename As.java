e4047f6f7cbfc9d1d85219eded42ff31b569c2a0
Tanishqua24 <91310415+Tanishqua24@users.noreply.github.com>
2021-09-28 16:28:16 +0530

    Add files via upload

---
+++ As.java
@@ -0,0 +1,13 @@
+
+public class As {
+
+		public static void main(String[] args) {
+			int i , num = 100, sum = 0;
+			for (i=1; i <= num; i++) {
+				sum = sum + i;
+			}
+	        System.out.println("Sum is "+ sum);
+		}
+
+	}
+
